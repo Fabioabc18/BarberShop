abaee99124bfa5ee861c4b346c7cd2a2dd8608ed
Fábio Cabral <fabio.brito.cabral@gmail.com>
2023-02-11 10:17:49 +0000

    Alteração na navbar.css

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,6 +2,12 @@ import React from "react";
 
 function Location() {
   return <div></div>;
+
+  /* const MyApp = () => (
+    <Wrapper apiKey={"AIzaSyBUhVa0pld-IYySgIRi6Ma9mgMYuy2L0Dw"}>
+      <MyMapComponent />
+    </Wrapper>
+  ); */
 }
 
 export default Location;
