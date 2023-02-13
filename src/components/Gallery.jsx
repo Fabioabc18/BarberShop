a868125267b916498f41c107182722365e759cf1
Fábio Cabral <fabio.brito.cabral@gmail.com>
2023-02-13 19:32:49 +0000

    Implementação da logica na galeria

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Gallery() {
-  return <div></div>;
+function Gallery(props) {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/gallery.json")
+      .then(response => response.json())
+      .then(data => setImages(data));
+  }, []);
+
+  return (
+    <div>
+      {images.map(photo => (
+        <img key={photo.image_id} src={photo.url} alt="" />
+      ))}
+    </div>
+  );
 }
 
 export default Gallery;
