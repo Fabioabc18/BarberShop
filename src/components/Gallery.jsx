c5477a9583e7f8eaa45bee9f52b01da3bd2fdc0c
Fábio Cabral <fabio.brito.cabral@gmail.com>
2023-02-20 16:49:10 +0000

    Alterações no layout da galeria, e algumas correções nas Api's

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,6 +4,13 @@ import "./gallery.css";
 function Gallery() {
   const [image, setImage] = useState([]);
   const [photo, setPhoto] = useState([]);
+  const [famousModal, setFamousModal] = useState(false);
+  const [entryModal, setEntryModal] = useState(true);
+  const [selectedFamous, setSelectedFamous] = useState(null);
+
+  const closeModal = () => {
+    setFamousModal(false);
+  };
 
   useEffect(() => {
     fetch("/api/famousgallery.json")
@@ -22,31 +29,102 @@ function Gallery() {
   }, []);
 
   return (
-    <div className="gallery">
-      <div>
-        {image && (
-          <div>
-            {image.map(fam => (
-              <div className="famous" key={fam.famous_id}>
-                <img src={fam.url} alt="" />
-                <h3>{fam.name}</h3>
-                <p>{fam.description}</p>
-              </div>
-            ))}
+    <div>
+      {entryModal && (
+        <div className="entry-modal">
+          <div className="entry-modal-container">
+            <img
+              src="/images/flexaacortar.jpg"
+              alt="Foto do Flexa Barber a cortar o cabelo"
+            />
+            <div className="entry-content">
+              <h2>Inspira-te em alguns cortes</h2>
+              <p>
+                Nesta Galeria encontrarás alguns modelos de corte. Tudo o que
+                faço é com enorme paixão. Dizem que quem corre por gosto não
+                cansa, então cá estou eu na corrida!{" "}
+                <strong>More than a barber.</strong>
+              </p>
+              <button className="entrybtn" onClick={() => setEntryModal(false)}>
+                Entrar
+              </button>
+            </div>
           </div>
-        )}
-      </div>
-      <div>
-        {photo && (
-          <div>
-            {photo.map(client => (
-              <div className="clients" key={client.image_id}>
-                <img src={client.url} alt="" />
-              </div>
-            ))}
+        </div>
+      )}
+
+      <main className="gallery">
+        <section className="gallery-items">
+          <div className="grid-container">
+            {!entryModal && (
+              <>
+                {image.map((fam, index) => (
+                  <div
+                    className="famous"
+                    key={fam.famous_id}
+                    onClick={() => {
+                      setSelectedFamous(index);
+                      setFamousModal(true);
+                    }}
+                  >
+                    <img src={fam.url} alt={fam.description} />
+                  </div>
+                ))}
+                {photo &&
+                  photo.map(client => (
+                    <div className="clients" key={client.image_id}>
+                      <img src={client.url} alt="exemplo de corte de cabelo" />
+                    </div>
+                  ))}
+              </>
+            )}
           </div>
-        )}
-      </div>
+        </section>
+      </main>
+
+      {famousModal && (
+        <div className="modal-famous">
+          <div className="modal-famous-container">
+            <div className="modal-famous-content">
+              <span onClick={() => setFamousModal(false)}>
+                <button className="close-btn" onClick={closeModal}>
+                  X
+                </button>
+              </span>
+              <img /* novo state + nova classe - confirmar */
+                className="modal-famous-img"
+                src={image[selectedFamous].url}
+                alt={image[selectedFamous].name}
+              />
+              <div>
+                <div className="inside-famous-image">
+                  {image.map((fam, index) => (
+                    <div
+                      key={fam.famous_id}
+                      onClick={() => setSelectedFamous(index)}
+                      className={`small-famous ${
+                        selectedFamous === index ? "selected" : ""
+                      }`}
+                    >
+                      <img
+                        className="image-container"
+                        src={fam.url}
+                        alt={fam.name}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="text-content">
+                {" "}
+                {/* vai trocando consoante a foto que user selecione */}
+                <h3>{image[selectedFamous].name}</h3>
+                <p>{image[selectedFamous].description}.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
