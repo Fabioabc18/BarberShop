37c6940d302e739405c0f6128834924a63538a4a
Fábio cabral <fabio.brito.cabral@gmail.com>
2023-02-25 13:26:18 +0000

    Inserção da lógica para automatização do slider

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./contacts.css";
 
 function About() {
+  const images = [
+    "/images/logo1.png",
+    "/images/logo2.png",
+    "/images/logo3.png",
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentImage(prevImage => {
+        prevImage++;
+        if (prevImage === images.length) {
+          prevImage = 0;
+        }
+
+        return prevImage;
+      });
+    }, 3000);
+  }, [images.length]);
+
   return (
-    <div>
-      <h1>teste</h1>
-    </div>
+    <main className="modal">
+      <div className="modal-content">
+        <div>
+          <img src={images[currentImage]} alt="" />
+        </div>
+        <div className="text">
+          <h1> Como cheguei aqui</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, at,
+            corrupti illum doloribus vero nobis voluptatem molestiae ipsum
+            minima maxime quisquam in dolore explicabo quod sit ipsa laboriosam
+            neque dolores reprehenderit. Officiis sequi eos totam debitis
+            doloremque vero voluptatem ex?
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
